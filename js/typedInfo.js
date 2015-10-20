08f6cee2da4812f2732bd0ea8e74ae3d39ebcb37
Hassanmir92 <hassanmir92@gmail.com>
2015-10-20 14:57:50 +0100

    styling changed, removed atoms and added typed.

---
+++ js/typedInfo.js
@@ -0,0 +1,9 @@
+$(".intro-typed").typed({
+  strings: ["Hey, I am Hassan Mir", "I am a web developer"],
+  typeSpeed: 100,
+  backDelay: 3500,
+  startDelay: 10,
+  loop: true,
+  loopCount: false,
+  cursorChar: "|"
+});
